202bf91a9b67bff93a4c663992d48b76802cdcfe
respectevery01 <2849288402@qq.com>
2025-03-02 15:18:38 +0800

    update

--- server.js
+++ server.js
@@ -5,7 +5,30 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 提供静态文件
+// Create a session tracker to remember which users have seen the intro
+const userSessions = new Set();
+
+// Serve static files except for main.html which we'll handle specially
+app.use('/main.html', (req, res, next) => {
+  // Check if this is a direct request for main.html
+  const referer = req.get('Referer');
+  const hasSeenIntro = referer && (
+    referer.includes('/index.html') || 
+    referer.endsWith('/') || 
+    userSessions.has(req.ip)
+  );
+  
+  if (hasSeenIntro) {
+    // User is coming from index.html, allow access to main.html
+    userSessions.add(req.ip); // Remember this user
+    next();
+  } else {
+    // User is trying to access main.html directly, redirect to index
+    res.redirect('/');
+  }
+});
+
+// Serve other static files normally
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API endpoint to get articles
@@ -73,17 +96,37 @@ app.get('/api/articles/:id', (req, res) => {
   }
 });
 
-// 主页路由
+// Home page route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// 终端主页路由
+// Terminal main page route
 app.get('/main', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'main.html'));
+  // Similar check as for /main.html
+  const referer = req.get('Referer');
+  const hasSeenIntro = referer && (
+    referer.includes('/index.html') || 
+    referer.endsWith('/')
+  );
+  
+  if (hasSeenIntro) {
+    // User is coming from index.html, allow access to main
+    userSessions.add(req.ip); // Remember this user
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+  } else {
+    // User is trying to access main directly, redirect to index
+    res.redirect('/');
+  }
 });
 
-// 启动服务器
+// Add a route to register when a user has seen the intro (called from index.html)
+app.get('/seen-intro', (req, res) => {
+  userSessions.add(req.ip);
+  res.status(200).send('OK');
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 }); 
